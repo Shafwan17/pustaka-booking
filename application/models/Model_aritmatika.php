f8de770c3ad0f40efcc24516f5022245168f5a2a
Shafwan17 <shafwankurniawan@gmail.com>
2023-04-01 14:54:17 +0700

    pustaka-booking

---
+++ application/models/Model_aritmatika.php
@@ -0,0 +1,15 @@
+<?php
+
+class Model_aritmatika extends CI_Model
+{
+    public $nilai1, $nilai2, $hasil;
+
+    public function penjumlahan($n1, $n2)
+    {
+        $nilai1 = $n1;
+        $nilai2 = $n2;
+        $hasil = $nilai1 + $nilai2;
+        echo $hasil;
+    }
+}
+        
---
+++ application/views/biodata.php
@@ -0,0 +1,3 @@
+Nama : Muhammad Shafwan Kurniawan
+Nim : 17200842
+Kelas : 17.6A.26
